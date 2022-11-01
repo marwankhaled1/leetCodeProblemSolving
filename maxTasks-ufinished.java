23675d42ce29904eac356b837d9f32b56fed4e2c
marwankhaled1 <49348053+marwankhaled1@users.noreply.github.com>
2022-11-01 13:26:56 +0200

    Create maxTasks-ufinished.java

---
+++ maxTasks-ufinished.java
@@ -0,0 +1,25 @@
+
+    public static int maxTasks(int []tasks,int time)
+    {
+
+        // create variable to save the potential tasks you Can Do
+        int numOfTaks=0;
+
+        // Sort the array of times in ascending order
+
+
+
+
+        // loop on array
+
+         //if time of task <= rest of the time you have
+             // update time you have and update #of tasks you can do
+
+
+
+
+        //returm # tasks you can do
+
+        return numOfTaks;
+    }
+
