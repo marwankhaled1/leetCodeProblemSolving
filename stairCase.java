5d2a903f117010f545b05f10a285394d2c47fec3
marwankhaled1 <49348053+marwankhaled1@users.noreply.github.com>
2022-11-04 16:27:44 +0200

    Create stairCase.java

---
+++ stairCase.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        stairCase(6);
+    }
+
+
+    public static void stairCase(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+
+            for(int j=n-i;j>0;j--){
+
+                System.out.print(" ");
+            }
+
+            for(int x=n-i;x<n;x++)
+            {
+
+                System.out.print("#");
+            }
+            System.out.println();
+
+        }
+
+
+
+    }
+}
